76678c95984e2e8f073f18d8d5745d2065db734f
monse <78182277+dmmonse@users.noreply.github.com>
2022-02-03 18:59:51 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let bestCake = "chocolate";
+let bestIcing = "coffee";
+
+
+//makeCake
+
+
+
+function makeCake(cake) {
+cakebatter (cake);
+frosting (icing);
+return bakeCake (cake, icing);
+}
+
+let bestCake = bakeCake(bestCake, bestIcing);
+
+console.log ()
